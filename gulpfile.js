88979979dd3435f0af4f43dd54d6ae9aa481acae
Vinicius Baptista <viniciusb_s@hotmail.com>
2024-12-25 20:36:43 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') //pega todos os arquivos .scss dentro dessa pasta
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); //define local que os arquivos de saida irão
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*') //pega todos as pastas e todos os arquivos dentro delas
+        .pipe(imagemin()) //define que o tipo de saida dos arquivos vai ser comprimido, melhora performance
+        .pipe(gulp.dest('./dist/images')); //define local que os arquivos de saida irão
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
